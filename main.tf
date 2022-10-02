0fb1a006b121c6a4e09da67e9dfc10bd61937dc3
Vitor Rezende <vit.rez@outlook.com>
2022-10-01 21:38:02 -0300

    minor bugs

--- main.tf
+++ main.tf
@@ -1,37 +1,37 @@
- terraform {
-    required_version = ">=1.0.0"
+terraform {
+  required_version = ">= 1.0.0"
 
-    required_providers {
-        aws = {
-            source = "hasicorp/aws"
-            version = "3.73.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
 
-    backend "s3" {
-        bucket = "vitor-remote-state"
-        key = "aws-vm/terraform.tfstate"
-        region = "eu-central-1"
+  backend "s3" {
+    bucket = "vitor-remote-state"
+    key    = "aws-vm/terraform.tfstate"
+    region = "sa-east-1"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+
+  default_tags {
+    tags = {
+      owned      = "vitorrezende"
+      managed-by = "terraform"
     }
- }
+  }
+}
 
- provider "aws" {
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "vitor-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
     region = "eu-central-1"
+  }
 
-    default_tags {
-        tags = {
-            owned = "vitorrezende"
-            managed-by = "terraform"
-        }
-    }
- }
- 
- data "terraform_remote_state" "vpc" {
-    backend = "s3"
-    config = {
-        bucket = "vitor-remote-state"
-        key = "aws-vpc/terraform.tfstate"
-        region = "eu-central-1"
-     }
-   
- }
+}
--- vm.tf
+++ vm.tf
@@ -1,19 +1,19 @@
 resource "aws-key-name" "key" {
-    key_name = "aws-key"
-    public_key = file(aws-key.pub)
+  key_name   = "aws-key"
+  public_key = file(aws-key.pub)
 
 }
 
 resource "aws_instance" "vm" {
-    ami = "asd"
-    instance_type = "t2.micro"
-    key_name = aws_key_pair.key.name
-    subnet_id = data.terraform_remote_state.vpc.output.subnet_id
-    vpc_security_group_ids = [date.terraform_remote_state.vpc.output.vpc_security_group_id]
-    associate_public_ip_address = true
+  ami                         = "asd"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.key.name
+  subnet_id                   = data.terraform_remote_state.vpc.output.subnet_id
+  vpc_security_group_ids      = [date.terraform_remote_state.vpc.output.vpc_security_group_id]
+  associate_public_ip_address = true
 
-    tags =  {
-        "Name" = "vm-terraform"
-    }
+  tags = {
+    "Name" = "vm-terraform"
+  }
 
 }
